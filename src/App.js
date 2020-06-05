9f30c5f1bf3f138db7b1d142ea567b6200a438d0
Vivien Walter <walter.vivien.k@gmail.com>
2020-06-05 15:45:04 +0200

    app rewritten

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Pokemons from "./components/pages/Pokemons";
@@ -7,28 +7,28 @@ import PokemonsDetail from "./components/pages/PokemonDetail";
 
 import "./App.css";
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <div className="container">
-            <Header />
-            <Route
-              exact
-              path="/"
-              render={() => (
-                <React.Fragment>
-                  <h1>Welcome?</h1>
-                </React.Fragment>
-              )}
-            />
-            <Route path="/pokemons" component={Pokemons} />
-            <Route path="/types" component={Types} />
-            <Route path="/pokemon/" component={PokemonsDetail} />
-          </div>
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <div className="container">
+          <Header />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <h1>Welcome?</h1>
+              </React.Fragment>
+            )}
+          />
+          <Route path="/pokemons" component={Pokemons} />
+          <Route path="/types" component={Types} />
+          <Route path="/pokemon/" component={PokemonsDetail} />
         </div>
-      </Router>
-    );
-  }
-}
+      </div>
+    </Router>
+  );
+};
+
+export default React.memo(App);
