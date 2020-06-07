76804822aa3749dfd795d909ea3b4edc40775d4a
Vivien Walter <walter.vivien.k@gmail.com>
2020-06-07 17:15:59 +0200

    added styles to types, as forgotten

--- src/components/TypeItem.js
+++ src/components/TypeItem.js
@@ -1,10 +1,25 @@
 import React from "react";
+import styled /*, { ThemeProvider, css }*/ from "styled-components";
+
+const Card = styled.div`
+  background: #f4c20d;
+  width: 200px;
+  margin: 10px;
+  box-shadow: 9px 9px 22px -8px rgba(0, 0, 0, 0.2);
+`;
+
+const Name = styled.div`
+  padding: 20px 20px;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  color: #3b4255;
+`;
 
 const TypeItem = (props) => {
   return (
-    <div className="pokeCard">
-      <div className="card-name">{props.type.name}</div>
-    </div>
+    <Card className="pokeCard">
+      <Name className="card-name">{props.type.name}</Name>
+    </Card>
   );
 };
 
